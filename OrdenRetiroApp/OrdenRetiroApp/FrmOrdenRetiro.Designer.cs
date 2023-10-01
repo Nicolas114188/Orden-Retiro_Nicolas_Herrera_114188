62d0d11c9175871e42befef1b69c75b425b3f0ec
Nicolas114188 <nicolasalberto.herrera@gmail.com>
2023-10-01 19:53:19 -0300

    Semana 4

--- OrdenRetiroApp/OrdenRetiroApp/FrmOrdenRetiro.Designer.cs
+++ OrdenRetiroApp/OrdenRetiroApp/FrmOrdenRetiro.Designer.cs
@@ -50,26 +50,27 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(78, 27);
+            label1.Location = new Point(89, 36);
             label1.Name = "label1";
-            label1.Size = new Size(41, 15);
+            label1.Size = new Size(50, 20);
             label1.TabIndex = 8;
             label1.Text = "Fecha:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(43, 71);
+            label2.Location = new Point(49, 95);
             label2.Name = "label2";
-            label2.Size = new Size(76, 15);
+            label2.Size = new Size(96, 20);
             label2.TabIndex = 9;
             label2.Text = "Responsable:";
             // 
             // textResponsable
             // 
-            textResponsable.Location = new Point(141, 68);
+            textResponsable.Location = new Point(161, 91);
+            textResponsable.Margin = new Padding(3, 4, 3, 4);
             textResponsable.Name = "textResponsable";
-            textResponsable.Size = new Size(351, 23);
+            textResponsable.Size = new Size(401, 27);
             textResponsable.TabIndex = 1;
             // 
             // dgvDetalleOrden
@@ -78,23 +79,29 @@
             dgvDetalleOrden.AllowUserToDeleteRows = false;
             dgvDetalleOrden.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvDetalleOrden.Columns.AddRange(new DataGridViewColumn[] { ColCodigo, ColMaterial, ColStock, ColCantidad, ColAcciones });
-            dgvDetalleOrden.Location = new Point(43, 176);
+            dgvDetalleOrden.Location = new Point(49, 235);
+            dgvDetalleOrden.Margin = new Padding(3, 4, 3, 4);
             dgvDetalleOrden.Name = "dgvDetalleOrden";
             dgvDetalleOrden.ReadOnly = true;
+            dgvDetalleOrden.RowHeadersWidth = 51;
             dgvDetalleOrden.RowTemplate.Height = 25;
-            dgvDetalleOrden.Size = new Size(495, 150);
+            dgvDetalleOrden.Size = new Size(566, 200);
             dgvDetalleOrden.TabIndex = 5;
+            dgvDetalleOrden.CellContentClick += dgvDetalleOrden_CellContentClick;
             // 
             // ColCodigo
             // 
             ColCodigo.HeaderText = "CODIGO";
+            ColCodigo.MinimumWidth = 6;
             ColCodigo.Name = "ColCodigo";
             ColCodigo.ReadOnly = true;
             ColCodigo.Visible = false;
+            ColCodigo.Width = 125;
             // 
             // ColMaterial
             // 
             ColMaterial.HeaderText = "MATERIAL";
+            ColMaterial.MinimumWidth = 6;
             ColMaterial.Name = "ColMaterial";
             ColMaterial.ReadOnly = true;
             ColMaterial.Width = 150;
@@ -102,34 +109,42 @@
             // ColStock
             // 
             ColStock.HeaderText = "STOCK";
+            ColStock.MinimumWidth = 6;
             ColStock.Name = "ColStock";
             ColStock.ReadOnly = true;
+            ColStock.Width = 125;
             // 
             // ColCantidad
             // 
             ColCantidad.HeaderText = "CANTIDAD";
+            ColCantidad.MinimumWidth = 6;
             ColCantidad.Name = "ColCantidad";
             ColCantidad.ReadOnly = true;
+            ColCantidad.Width = 125;
             // 
             // ColAcciones
             // 
             ColAcciones.HeaderText = "ACCIONES";
+            ColAcciones.MinimumWidth = 6;
             ColAcciones.Name = "ColAcciones";
             ColAcciones.ReadOnly = true;
+            ColAcciones.Width = 125;
             // 
             // cboMaterial
             // 
             cboMaterial.FormattingEnabled = true;
-            cboMaterial.Location = new Point(43, 125);
+            cboMaterial.Location = new Point(49, 167);
+            cboMaterial.Margin = new Padding(3, 4, 3, 4);
             cboMaterial.Name = "cboMaterial";
-            cboMaterial.Size = new Size(219, 23);
+            cboMaterial.Size = new Size(250, 28);
             cboMaterial.TabIndex = 2;
             // 
             // btnAgregar
             // 
-            btnAgregar.Location = new Point(439, 123);
+            btnAgregar.Location = new Point(502, 164);
+            btnAgregar.Margin = new Padding(3, 4, 3, 4);
             btnAgregar.Name = "btnAgregar";
-            btnAgregar.Size = new Size(75, 23);
+            btnAgregar.Size = new Size(86, 31);
             btnAgregar.TabIndex = 4;
             btnAgregar.Text = "Agregar";
             btnAgregar.UseVisualStyleBackColor = true;
@@ -137,44 +152,50 @@
             // 
             // btnAceptar
             // 
-            btnAceptar.Location = new Point(206, 415);
+            btnAceptar.Location = new Point(235, 553);
+            btnAceptar.Margin = new Padding(3, 4, 3, 4);
             btnAceptar.Name = "btnAceptar";
-            btnAceptar.Size = new Size(75, 23);
+            btnAceptar.Size = new Size(86, 31);
             btnAceptar.TabIndex = 6;
             btnAceptar.Text = "Aceptar";
             btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
             // 
             // btnCancelar
             // 
-            btnCancelar.Location = new Point(325, 415);
+            btnCancelar.Location = new Point(371, 553);
+            btnCancelar.Margin = new Padding(3, 4, 3, 4);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.Size = new Size(86, 31);
             btnCancelar.TabIndex = 7;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
             // 
             // textFecha
             // 
             textFecha.Enabled = false;
-            textFecha.Location = new Point(141, 25);
+            textFecha.Location = new Point(161, 33);
+            textFecha.Margin = new Padding(3, 4, 3, 4);
             textFecha.Name = "textFecha";
-            textFecha.Size = new Size(95, 23);
+            textFecha.Size = new Size(108, 27);
             textFecha.TabIndex = 0;
             // 
             // nudCantidad
             // 
-            nudCantidad.Location = new Point(294, 125);
+            nudCantidad.Location = new Point(336, 167);
+            nudCantidad.Margin = new Padding(3, 4, 3, 4);
             nudCantidad.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             nudCantidad.Name = "nudCantidad";
-            nudCantidad.Size = new Size(120, 23);
+            nudCantidad.Size = new Size(137, 27);
             nudCantidad.TabIndex = 10;
             nudCantidad.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // FrmOrdenRetiro
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(615, 447);
+            ClientSize = new Size(701, 585);
             Controls.Add(nudCantidad);
             Controls.Add(textFecha);
             Controls.Add(btnCancelar);
@@ -185,8 +206,9 @@
             Controls.Add(textResponsable);
             Controls.Add(label2);
             Controls.Add(label1);
-            MaximumSize = new Size(631, 486);
-            MinimumSize = new Size(631, 486);
+            Margin = new Padding(3, 4, 3, 4);
+            MaximumSize = new Size(719, 632);
+            MinimumSize = new Size(719, 632);
             Name = "FrmOrdenRetiro";
             Text = "Registrar Orden Retiro";
             Load += FrmOrdenRetiro_Load;
--- OrdenRetiroApp/OrdenRetiroApp/FrmOrdenRetiro.cs
+++ OrdenRetiroApp/OrdenRetiroApp/FrmOrdenRetiro.cs
@@ -43,10 +43,13 @@ namespace OrdenRetiroApp
         {
             if (Validar())
             {
-                foreach (DataGridView row in dgvDetalleOrden.Rows)
+                foreach (DataGridViewRow row in dgvDetalleOrden.Rows)
                 {
-                    MessageBox.Show("Este material ya está detallado...", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                    return;
+                    if (row.Cells["ColMaterial"].Value.ToString().Equals(cboMaterial.Text))
+                    {
+                        MessageBox.Show("Este material ya está detallado...", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
                 }
                 Material material = (Material)cboMaterial.SelectedItem;
                 int cant = int.Parse(nudCantidad.Value.ToString());
@@ -57,6 +60,7 @@ namespace OrdenRetiroApp
                 }
                 DetalleOrden detalle = new DetalleOrden(material, cant);
                 ordenRetiro.AgregarDetalle(detalle);
+                dgvDetalleOrden.Rows.Add(new object[] { material.Codigo, material.Nombre, material.Stock, cant, "Quitar" });
             }
             else
             {
@@ -71,6 +75,44 @@ namespace OrdenRetiroApp
             if (cboMaterial.SelectedIndex == -1 || string.IsNullOrEmpty(textResponsable.Text))
                 val = false;
             return val;
+        }
+
+        private void dgvDetalleOrden_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvDetalleOrden.CurrentCell.ColumnIndex == 4)
+            {
+                ordenRetiro.QuitarDetalle(dgvDetalleOrden.CurrentRow.Index);
+                dgvDetalleOrden.Rows.RemoveAt(dgvDetalleOrden.CurrentRow.Index);
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (dgvDetalleOrden.Rows.Count != 0)
+            {
+                ordenRetiro.Fecha = Convert.ToDateTime(textFecha.Text);
+                ordenRetiro.Responsable = textResponsable.Text;
+                ordenRetiro.NroOrden = servicio.CrearOrdenRetiro(ordenRetiro);
+                if (ordenRetiro.NroOrden != 0)
+                {
+                    MessageBox.Show("Se registró con éxito la Orden de Retiro: "+ordenRetiro.NroOrden.ToString(), "Informe", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    this.Dispose();
+                }
+                else
+                {
+                    MessageBox.Show("ERROR NO se pudo registrar...!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalido no ingreso correctamente Materiales...", "Control", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
         }
     }
 }
